03b2988a645598b1441f6e2006d40af4e72e5666
Arpitananda123 <arpitananda.11th.a@gmail.com>
2025-01-09 09:52:16 +0530

    Day-39

---
+++ Day-39/Day39.java
@@ -0,0 +1,15 @@
+public class Day39 {
+        public static int theMaximumAchievableX(int num, int t) {
+            return num+2*t;
+            
+        }
+        public static void main(String[] args){
+            int num=4;
+            int t=1;
+            int result=theMaximumAchievableX(num,t);
+            System.out.print(result);
+            
+         }
+     
+    
+}
