79b51594574c6775ca08358586bdd0b5069f746d
Arpitananda123 <arpitananda.11th.a@gmail.com>
2025-01-16 19:20:05 +0530

    Day-45

---
+++ Day-45/Day45.java
@@ -0,0 +1,11 @@
+public class Day45 {
+        public static int numberOfMatches(int n) {
+            return n-1;
+            
+        }
+        public static void main(String[] args){
+            int n=7;
+            System.out.println(numberOfMatches(n));
+        }
+    
+}
