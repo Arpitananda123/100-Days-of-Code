f42156722240334a3afc465d0b419eb3710a8d15
Arpitananda123 <arpitananda.11th.a@gmail.com>
2025-02-26 09:32:06 +0530

    Day-77

---
+++ Day-77/Day77.java
@@ -0,0 +1,10 @@
+public class Day77 {
+        public static boolean divisorGame(int n) {
+            return n%2==0;
+        }
+        public static void main(String[] args){
+            int n=9;
+            System.out.println(divisorGame(n));
+        }
+    
+}
